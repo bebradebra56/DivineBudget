dc23e0c84d3f014e8d33a2c5385dc7014d679c56
fraim <fraimework@gmail.com>
2025-11-18 16:37:01 +0700

    update design

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,30 @@ android {
         }
     }
 
+    signingConfigs {
+        create("release") {
+            val isCI = System.getenv("CI") == "true"
+            if (isCI) {
+                val keystorePath = System.getenv("CM_KEYSTORE_PATH")
+                val keystorePassword = System.getenv("CM_KEYSTORE_PASSWORD")
+                val keyAlias = System.getenv("CM_KEY_ALIAS")
+                val keyPassword = System.getenv("CM_KEY_PASSWORD")
+
+                if (keystorePath.isNullOrEmpty()) {
+                    throw GradleException("CM_KEYSTORE_PATH is not set!")
+                }
+
+                storeFile = file(keystorePath)
+                storePassword = keystorePassword
+                this.keyAlias = keyAlias
+                this.keyPassword = keyPassword
+            }
+        }
+    }
+
     buildTypes {
         release {
+            signingConfig = signingConfigs.getByName("release")
             isMinifyEnabled = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
@@ -92,11 +114,11 @@ dependencies {
     implementation("androidx.activity:activity-compose:1.8.1")
     
     // Compose
-    implementation(platform("androidx.compose:compose-bom:2023.10.01"))
+    implementation(platform("androidx.compose:compose-bom:2025.11.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3:1.1.2")
+    implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.material:material-icons-extended")
     
     // Navigation
@@ -129,7 +151,7 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.10.01"))
+    androidTestImplementation(platform("androidx.compose:compose-bom:2024.09.03"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
